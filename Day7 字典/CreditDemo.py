1c03bb249bbe1e5cb246c44d492dbef048ff41cc
v_shevzliu <v_shevzliu@tencent.com>
2020-10-28 11:58:59 +0800

    add file seven

--- Day7 字典/CreditDemo.py
+++ Day7 字典/CreditDemo.py
@@ -1,8 +1,10 @@
 class Credit:
-    def __init__(self,cardNum,password='123456'):
-        if password=='123456':
-            print('信用卡'+cardNum+'的默认密码为'+password)
+    def __init__(self, cardNum, password="123456"):
+        if password == "123456":
+            print("信用卡" + cardNum + "的默认密码为" + password)
         else:
-            print('重置信用卡'+cardNum+'的密码为'+password)
-Credit('421087199403247312')
-Credit('421087199403247312','4396')
+            print("重置信用卡" + cardNum + "的密码为" + password)
+
+
+Credit("421087199403247312")
+Credit("421087199403247312", "1345628")
